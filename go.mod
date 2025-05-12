100eb4d716b4a8ce4ffaa35fb3335a9d488c692b
Matthew Davidson <matthewd0224@gmail.com>
2025-05-12 21:25:12 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/voltracker/trace-router
+
+go 1.23.2
+
+require github.com/google/gopacket v1.1.19 // indirect
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+)
+
+type Hop struct {
+	src     string
+	dest    string
+	latency float64
+}
+
+type TraceResult struct {
+	initialSrc string
+	finalDst   string
+	hops       []Hop
+}
+
+func getHandle() *pcap.Handle {
+	if os.Getenv("TRACE_ROUTER_LIVE") != "1" {
+		if handle, err := pcap.OpenOffline(os.Getenv("TRACE_ROUTER_INPUT_FILE")); err != nil {
+			panic(err)
+		} else {
+			return handle
+		}
+	}
+	if handle, err := pcap.OpenLive(os.Getenv("TRACE_ROUTER_INTERFACE"), 1600, true, pcap.BlockForever); err != nil {
+		panic(err)
+	} else {
+		return handle
+	}
+}
+
+func parseOutput(output string) []Hop {
+	var lines = strings.Split(output, "\n")
+	var hops = []Hop{}
+	for line := range lines {
+		if !strings.Contains(lines[line], "*") {
+			split := strings.Fields(lines[line])
+			if len(split) > 2 {
+				val, err := strconv.ParseFloat(split[2], 32)
+				if err != nil {
+					slog.Error("failed to parse float from traceroute output")
+				}
+				var src string
+				src = "localhost"
+				if len(hops) >= 1 {
+					src = hops[len(hops)-1].dest
+				}
+				hops = append(hops, Hop{
+					src:     src,
+					dest:    split[1],
+					latency: val,
+				})
+			}
+		}
+	}
+	return hops
+}
+
+func runTraceroute(src string, dst string, channel chan TraceResult) {
+	out, err := exec.Command("/usr/sbin/traceroute", "-n", "-q 1", dst).Output()
+	if err != nil {
+		slog.Error("failed to execute command", "error", err)
+	}
+	hops := parseOutput(string(out))
+	slog.Info("finished traceroute for " + dst + "...")
+	channel <- TraceResult{initialSrc: src, finalDst: dst, hops: hops}
+}
+
+func handlePacket(packet gopacket.Packet, wg *sync.WaitGroup, channel chan TraceResult) {
+	ipLayer := packet.Layer(layers.LayerTypeIPv4)
+	if ipLayer != nil {
+		ipv4, ok := ipLayer.(*layers.IPv4)
+		if ok && !ipv4.DstIP.IsPrivate() {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				runTraceroute("localhost", ipv4.DstIP.String(), channel)
+			}()
+			slog.Info("starting traceroute to " + ipv4.DstIP.String() + "...")
+		}
+	}
+}
+
+func main() {
+	handle := getHandle()
+	var wg sync.WaitGroup
+	channel := make(chan TraceResult)
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		handlePacket(packet, &wg, channel)
+	}
+	wg.Wait()
+	slog.Info("finished running traceroutes...")
+	close(channel)
+	for res := range channel {
+		slog.Info("Traceroute for " + res.initialSrc + " -> " + res.finalDst)
+		for i, hop := range res.hops {
+			slog.Info("Hop "+strconv.Itoa(i)+": ", "src", hop.src, "dest", hop.dest, "latency", hop.latency)
+		}
+	}
+}
