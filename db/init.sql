3283250d1bdf578db17f0d9c3056afaeca8708a2
Matthew Davidson <matthewd0224@gmail.com>
2025-05-20 00:06:50 +0100

    added view for aggregating hops
    this isn't crazy efficent but let's be honest this isn't exactly a web scale app is it

--- db/init.sql
+++ db/init.sql
@@ -5,3 +5,12 @@ CREATE TABLE hops (
   latency DOUBLE PRECISION,
   time_added TIMESTAMP DEFAULT NOW()
 );
+
+CREATE VIEW hops_agg AS
+SELECT
+  source_ip,
+  dest_ip,
+  COUNT(*)       AS count,
+  AVG(latency)   AS avg_latency
+FROM hops 
+GROUP BY source_ip, dest_ip;
